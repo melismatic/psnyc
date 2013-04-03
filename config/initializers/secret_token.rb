4cd2bd43764611afde1a6309b9b26c1241d015a3
M Corbett <msmcorbett@gmail.com>
2013-04-03 00:06:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psnyc::Application.config.secret_token = '22eb744478741fa75998c015d80a4cdd15ab6aacc6bdf0986072267cb2b80fe3a9c0ac2215928a47d1382269ff12514416e5fae172508486b9078581ab73d6fc'
